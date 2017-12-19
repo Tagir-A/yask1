a6870c16603db7b852a4d23a0db278d42ff884a6
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2017-12-20 00:47:17 +0300

    fix(query): fix a typo

--- graphql/resolvers/query.js
+++ graphql/resolvers/query.js
@@ -5,18 +5,18 @@ event (root, { id }) {
     return models.Event.findById(id);
   },
   events (root, args, context) {
-    return models.Event.findAll(argumets, context);
+    return models.Event.findAll(args, context);
   },
 user (root, { id }) {
     return models.User.findById(id);
   },
   users (root, args, context) {
-    return models.User.findAll({}, context);
+    return models.User.findAll(args, context);
   },
 room (root, { id }) {
     return models.Room.findById(id);
   },
   rooms (root, args, context) {
-    return models.Room.findAll({ offset: 1 }, context);
+    return models.Room.findAll(args, context);
   }
 };
